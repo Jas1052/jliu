bb9234b97cf6a53d6e7d3d43c11655e3562798f0
Jas1052 <jliu1052@gmail.com>
2017-12-20 14:45:26 -0600

    added social buttons and links

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -1,7 +1,7 @@
-window.sr = ScrollReveal({ reset: true });
+window.sr = ScrollReveal({ reset: true, viewFactor: '0.25' });
 
 sr.reveal('.reveal', { duration: 1000 });
-sr.reveal('.slow-reveal', { duration: 1500 });
+sr.reveal('.slow-reveal', { duration: 1000});
 
 sr.reveal('.pattern-reveal-1', { duration: 1500 });
 sr.reveal('.pattern-reveal-2', { duration: 2000 });
