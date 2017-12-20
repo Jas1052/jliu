9c0d66fe37a3a4e55eb1bdb042545a5aef82a4fa
Jas1052 <jliu1052@gmail.com>
2017-12-20 02:04:34 -0600

    update on website, new wection

---
+++ js/scrollreveal.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.reveal', { duration: 1000 });
+sr.reveal('.slow-reveal', { duration: 1500 });
+
+sr.reveal('.pattern-reveal-1', { duration: 1500 });
+sr.reveal('.pattern-reveal-2', { duration: 2000 });
+sr.reveal('.pattern-reveal-3', { duration: 2500 });
